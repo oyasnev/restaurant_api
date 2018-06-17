a0c7696b1b5347830f24dacc007df91e08455d9a
Oleg Yasnev <oleg.yasnev@knime.com>
2018-06-17 23:26:27 +0200

    get by id

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,21 +6,32 @@ router.post('/add', async (ctx) => {
     try {
         const req = ctx.request.body;
         const newRest = new Restaurant();
-        newRest.name = req.name;        
+        newRest.name = req.name;
         newRest.address = req.address;
         newRest.cuisine = req.cuisine || [];
         newRest.lon = req.lon;
         newRest.lat = req.lat;
-        
+
         await newRest.save();
 
-        ctx.status = 201;
         ctx.body = {
             status: 'success',
             data: newRest
         }
     } catch (err) {
         console.log(err);
+    }
+});
+
+router.get('/get/:id', async (ctx) => {
+    try {
+        const rest = await Restaurant.findById(ctx.params.id);
+        ctx.body = {
+            status: 'success',
+            data: rest
+        };
+    } catch (err) {
+        console.log(err)
     }
 })
 
